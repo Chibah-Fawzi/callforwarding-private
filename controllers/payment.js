70e6ca5687ca6117e282f9e8c8d6a6a3a7828481
chibah-fawzi <fawzichibah@gmail.com>
2022-10-05 21:42:04 +0100

    added check available numbers, buy a number and get users numbers

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,5 +1,10 @@
 const { STRIPE_SECRET_KEY, STRIPE_DOMAIN } = process.env;
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
+const Vonage = require("@vonage/server-sdk");
+const vonage = new Vonage({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+});
 
 async function createPayment(req, res) {
   const { price } = req.body;
@@ -13,15 +18,15 @@ async function createPayment(req, res) {
     },
   });
 
-  // const confirmPaymentIntent = await stripe.paymentIntents.confirm(
-  //   paymentIntent.id,
-  //   { payment_method: "pm_card_visa", return_url: "http://localhost:3000" }
-  // );
+  const confirmPaymentIntent = await stripe.paymentIntents.confirm(
+    paymentIntent.id,
+    { payment_method: "pm_card_visa", return_url: "http://localhost:3000" }
+  );
 
   res.status(200).json({
     success: true,
     clientSecret: paymentIntent.client_secret,
-    // confirmPaymentIntent,
+    confirmPaymentIntent,
   });
 }
 async function getPayment(req, res) {
@@ -34,7 +39,87 @@ async function getPayment(req, res) {
   });
 }
 
+function checkNumber(req, res) {
+  const {
+    COUNTRY_CODE,
+    VONAGE_NUMBER_TYPE,
+    NUMBER_SEARCH_CRITERIA,
+    NUMBER_SEARCH_PATTERN,
+    VONAGE_NUMBER_FEATURES,
+  } = req.body;
+
+  vonage.number.search(
+    COUNTRY_CODE,
+    {
+      type: VONAGE_NUMBER_TYPE, // STRING => MOBILE - LANDLINE - TOLL FREE --- DEFAULT "MOBILE"
+      pattern: NUMBER_SEARCH_CRITERIA, // NUMBER => Area code || Preferred digits (OBLIGATORY)
+      search_pattern: NUMBER_SEARCH_PATTERN, // NUMBER => 0 : Starts With, 1 : Contains, 2 : Ends With --- DEFAULT : 0
+      features: VONAGE_NUMBER_FEATURES, // STRING => SMS - VOICE - MMS --- DEFAULT : "VOICE" - "SMS"
+    },
+    (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(
+          `Here are ${res.numbers.length} of the ${res.count} matching numbers available for purchase:`
+        );
+        res.numbers.forEach((number) => {
+          console.log(`Tel: ${number.msisdn} Cost: ${number.cost}`);
+        });
+      }
+    }
+  );
+}
+
+function buyNumber(req, res) {
+  const { COUNTRY_CODE, VONAGE_NUMBER } = req.body;
+
+  vonage.number.buy(COUNTRY_CODE, VONAGE_NUMBER, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.json({
+        success: false,
+        err,
+      });
+    } else {
+      console.log(JSON.stringify(result, null, 2));
+      res.json({
+        success: true,
+        result,
+      });
+    }
+  });
+}
+
+async function getNumbers(req, res) {
+  const { NUMBER_SEARCH_CRITERIA, NUMBER_SEARCH_PATTERN } = req.body;
+
+  vonage.number.get(
+    NUMBER_SEARCH_CRITERIA && NUMBER_SEARCH_PATTERN
+      ? {
+          pattern: NUMBER_SEARCH_CRITERIA, // NUMBER => Area code &&|| Preferred digits
+          search_pattern: NUMBER_SEARCH_PATTERN, // NUMBER => 0 : Starts With, 1 : Contains, 2 : Ends With,
+        }
+      : {},
+    (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(
+          `Here are ${res.numbers.length} of your ${res.count} matching numbers:`
+        );
+        res.numbers.forEach((number) => {
+          console.log(`Tel: ${number.msisdn} Cost: ${number.type}`);
+        });
+      }
+    }
+  );
+}
+
 module.exports = {
   createPayment,
   getPayment,
+  buyNumber,
+  checkNumber,
+  getNumbers,
 };
--- routes/payment.js
+++ routes/payment.js
@@ -4,8 +4,9 @@ function init(router) {
   router.route("/createPayment").post(paymentController.createPayment);
   router.route("/getPayment/:id").get(paymentController.getPayment);
   //   router.route("/payment").post(paymentController.getPayment);
-  //   router.route("/number").get(paymentController.getNumber);
-  //   router.route("/number").post(paymentController.createNumber);
+  router.route("/numbers").get(paymentController.getNumbers);
+  router.route("/number").post(paymentController.buyNumber);
+  router.route("/availableNumbers").get(paymentController.checkNumber);
   //   router.route("/customers").post(paymentController.createCustomerInfos);
   //   router.route("/customer/:id").get(paymentController.getCustomerInfos);
   //   router.route("/customers").get(paymentController.getCustomersInfos);
